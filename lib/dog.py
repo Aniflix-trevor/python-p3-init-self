4fb15f0c4f65e42f8d2451d18a8831ed9b1bd9e8
Trevor <nyigeitrevor@gmail.com>
2025-05-17 11:47:35 +0300

    innit

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
+
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        return f"{self.name} says Woof!"
+
+    def breed_info(self):
+        return f"{self.name} is a {self.breed}."
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
+
+
 class Person:
-    pass
+    def __init__(self, name, age=None):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        return f"Hello, my name is {self.name}."
+
+    def age_info(self):
+            return f"I am {self.age} years old."
+  
+        
+
+
